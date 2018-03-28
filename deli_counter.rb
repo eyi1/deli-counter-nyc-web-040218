9e8b3e2c13ca470c10d0e819b32dada9a59d0613
eyi1 <ej7729@gmail.com>
2018-03-28 02:51:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(line)
   if line.length == 0
-    puts "There is nobody waiting to be served"
+    puts "There is nobody waiting to be served!"
   elsif line.length > 0
     puts "Current serving #{array.shift}."
   end
