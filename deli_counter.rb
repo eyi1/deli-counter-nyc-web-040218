f48c8b84760fcc25ec576b798edd3a02024fb34e
eyi1 <ej7729@gmail.com>
2018-03-28 02:30:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 def line (array)
   if array.length == 0
     puts "The line is currently empty."
-  else 
+  else
     message = "The line is currently:"
-    array.each_with_index {|name, index| message += "#{index+1}. #{name}"
-  end 
+    array.each_with_index do {|name, index| message += "#{index+1}. #{name}"
+  end
   puts message
 end
-  
+
 
 end
 # Write your code here.
